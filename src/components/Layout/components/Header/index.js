479d721c3552ada278dd1423c955b893313e0724
DinhNhat20 <2151050301nhat@ou.edu.vn>
2024-07-22 21:37:24 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles); //Cách sử dụng được ký tự "-" cho class name
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                {/* Search */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
